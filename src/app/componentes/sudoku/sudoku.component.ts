bfd6c0e9555ca75db2a64ac178b96bbb497b1259
fsaibene <fernandomsaibene@hotmail.com>
2018-09-06 01:46:17 -0300

    correccion

--- src/app/componentes/sudoku/sudoku.component.ts
+++ src/app/componentes/sudoku/sudoku.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sudoku',
-  templateUrl: './sudoku.component.html']
+  templateUrl: './sudoku.component.html',
+  styleUrls: ['./sudoku.component.css']
 })
 export class SudokuComponent implements OnInit {
 
